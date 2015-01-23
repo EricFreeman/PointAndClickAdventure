ff4401544d05f078f3aac1e1268a3a5f56fd5b69
Eric Freeman <eric.freeman@extendhealth.com>
2015-01-22 22:13:29 -0700

    Cool, it works with 4.3 sprites instead of new gui.

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Director : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                 var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                 var hitCollider = Physics2D.OverlapPoint(mousePosition);
+
+                if (hitCollider)
+                {
+                    var t = hitCollider.GetComponent<GoToRoom>();
+                    if (t != null)
+                        t.Go();
+                }
+            }
+        }
+    }
+}

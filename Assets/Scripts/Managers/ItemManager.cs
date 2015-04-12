48b1cb97fe0c27119a022a156a0e1b62d10b3d49
Eric Freeman <clownhunter@gmail.com>
2015-04-12 15:10:12 -0600

    Items won't spawn again after you've taken them.

--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -5,40 +5,47 @@ using System.Reflection;
 using Assets.Scripts.Interfaces;
 using Assets.Scripts.Items;
 using Assets.Scripts.Messages;
+using Assets.Scripts.Util;
 using UnityEngine;
 using UnityEventAggregator;
 
 namespace Assets.Scripts.Managers
 {
     public class ItemManager : MonoBehaviour,
-        IListener<EnterRoomMessage>
+        IListener<EnterRoomMessage>,
+        IListener<TakeItemMessage>
     {
         public GameObject BaseItem;
 
         private Dictionary<string, List<IItem>> _roomItems;
 
+        private List<IItem> _inventory;
+
         void Start()
         {
             _roomItems = new Dictionary<string, List<IItem>>();
+            _inventory = new List<IItem>();
 
             Assembly.GetExecutingAssembly().GetTypes()
                 .Where(type => type.GetInterface("IItem") != null && !type.IsInterface)
                 .Each(item =>
                 {
-                    var obj = (IItem) Activator.CreateInstance(item);
-                    
+                    var obj = (IItem)Activator.CreateInstance(item);
+
                     if (!_roomItems.ContainsKey(obj.Room))
                         _roomItems.Add(obj.Room, new List<IItem>());
-                    
+
                     _roomItems[obj.Room].Add(obj);
                 });
 
             this.Register<EnterRoomMessage>();
+            this.Register<TakeItemMessage>();
         }
 
         void OnDestroy()
         {
             this.UnRegister<EnterRoomMessage>();
+            this.UnRegister<TakeItemMessage>();
         }
 
         public void Handle(EnterRoomMessage message)
@@ -52,15 +59,20 @@ namespace Assets.Scripts.Managers
             });
 
             // add items from entered room
-
             if (!_roomItems.ContainsKey(message.RoomName)) return;
 
             _roomItems[message.RoomName].Each(item =>
             {
-                var baseItem = ((GameObject)Instantiate(BaseItem)).GetComponent<BaseItem>();
+                if (_inventory.ContainsType(item)) return;
+                var baseItem = Instantiate(BaseItem).GetComponent<BaseItem>();
                 baseItem.Setup(item);
                 baseItem.transform.SetParent(itemParent.transform);
             });
+        }
+
+        public void Handle(TakeItemMessage message)
+        {
+            _inventory.Add(message.Item);
         }
     }
 }
--- Assets/Scripts/Util/Extensions.cs
+++ Assets/Scripts/Util/Extensions.cs
@@ -1,10 +1,27 @@
-﻿namespace Assets.Scripts.Util
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Util
 {
     public static class Extensions
     {
         public static string ToFormat(this string s, params object[] p)
         {
             return string.Format(s, p);
+        }
+
+        public static bool ContainsType<T>(this List<T> list, T item)
+        {
+            var contains = false;
+
+            list.ForEach(listItem =>
+            {
+                if (listItem.GetType().Name == item.GetType().Name)
+                {
+                    contains = true;
+                }
+            });
+
+            return contains;
         }
     }
 }
